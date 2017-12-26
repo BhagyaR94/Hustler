067a8758907d277913b7fc90458c2643c79169a5
Bhagya Rathnayake <bhagya@omobio.net>
2017-12-26 11:59:54 +0530

    initial commit

---
+++ Hustler/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text, AppRegistry} from 'react-native';
+
+const App = () => (
+    <View>
+        <Text style={{color:'#998877'}}>HW</Text>
+    </View>
+);
+
+
+AppRegistry.registerComponent('Hustler', () => App);
